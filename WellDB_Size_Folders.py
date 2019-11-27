7c39a7fe325a239185b6396ab5851181ed40db67
Thomas Greig <55128878+Greigstar@users.noreply.github.com>
2019-11-27 09:27:21 +0100

    Add files via upload
    updated with Mauritania

--- WellDB_Size_Folders.py
+++ WellDB_Size_Folders.py
@@ -2,7 +2,7 @@ import locale
 import os
 import sys
 #folder = r'D:\Python\Sub_Appl_Data\WellDB'
-folder = r'G:\Sub_Appl_Data\WellDB\AR\wells'
+folder = r'G:\Sub_Appl_Data\WellDB\MR'
 Folder_of_interest=['00.Original_Vendor_Data','01.Well_Planning','02.Drilling_and_Completion','03.Directional_Surveys','04.Mud_Logs','05.LWD_Log_data','06.Wireline_Log_Data','07.Borehole_Seismic','08.Formation_Pressure_Data','09.Well_Test_Data','10.Fluid_Data','11.Core_Data','12.Geology_Data_and_Evaluations','13.Petrophysical_Data_Evaluations','14.Final_Well_Report_and_Completion_Log','15.Production_Logs','16.Production','99.Miscellaneous']
 locale.setlocale(locale.LC_ALL, "")
 
@@ -11,7 +11,7 @@ locale.setlocale(locale.LC_ALL, "")
 class Logger(object):
     def __init__(self):
         self.terminal = sys.stdout
-        self.log = open("D:\Python\Scripts\WellDBData\Welldb_size_AR.txt", "a")
+        self.log = open("D:\Python\Scripts\WellDBData\Welldb_size_MR.txt", "a")
 
     def write(self, message):
         self.terminal.write(message)
@@ -67,5 +67,4 @@ for root, dirs, files in os.walk(folder):
             for item in Folder_of_interest:
                 if item == result and f_match != 1:
                     f_match=1
-
                     print root, "size ; ", print_sizes(root, f_match)
